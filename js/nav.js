18f87deb298d92e2714c2b33b0026fee4f9669e0
xmandx <amanda.godinho@etec.sp.gov.br>
2024-02-25 17:08:36 -0300

    leaving it responsive

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,16 @@
 const btn = document.querySelector('nav button');
 const menu = document.querySelector('.menu');
-const btnclose = document.querySelector('.nav button');
+const btnclose = document.querySelector('.btnclose');
+const out = document.querySelector('.menu>button');
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
 btnclose.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+out.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
